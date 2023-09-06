ccde8d0fa3d5950d2beee15b19129e46d2325af4
Vasili <ballov_v@mail.ru>
2023-09-06 22:32:14 +0300

    fixed eslint errors

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 import '../scss/styles.scss';
-// eslint-disable-next-line no-unused-vars
-import * as bootstrap from 'bootstrap';
+import 'bootstrap';
 import app from './application.js';
 
 app();
